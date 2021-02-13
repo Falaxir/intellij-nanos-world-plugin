a532d9da548c384e2ef1397f06c14b1dcba5c846
GitHub Action <action@github.com>
2021-02-13 11:38:41 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/falaxir/intellijnanosworldplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.falaxir.intellijnanosworldplugin.services
+
+import com.github.falaxir.intellijnanosworldplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/falaxir/intellijnanosworldplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/falaxir/intellijnanosworldplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.falaxir.intellijnanosworldplugin.services
 
+import com.github.falaxir.intellijnanosworldplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
